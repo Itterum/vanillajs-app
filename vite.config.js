8476553238786f799578e5f1a3972165ea101a8c
lyashenko.ivan <ivan.lyashenko.it@gmail.com>
2024-07-11 23:02:51 +0300

    next try

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,5 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite';
 
 export default defineConfig({
-    plugins: [
-    ],
-    build: {
-        outDir: '../dist',
-        sourcemap: true
-    },
-    server: {
-        open: true,
-        port: 4000
-    },
-    root: 'src',
-    base: '/vanillajs-app/'
-})
+    base: '/vanillajs-app/',
+});
